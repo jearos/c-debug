d1fdbfbdc115c8b412c4fe71715a4399cce1a207
Jean Roström <jean@rostrom.se>
2019-08-24 09:56:04 +0200

    Add call with returns

--- c-debug-test.c
+++ c-debug-test.c
@@ -5,11 +5,16 @@ void test_call(void)
     printf("Inside function\n");
 }
 
+unsigned int test_call_return(void)
+{
+    return 0xC0FFEE;
+}
 
 int main(void)
 {
     unsigned int test = 0xDEADBEEF;
     C_DEBUG_PRINT_X32(test);
-    C_DEBUG_PRINT_CALL(test_call());
+    C_DEBUG_PRINT_CALL_RETURN_X32(test = test_call_return());
+    C_DEBUG_PRINT_CALL_RETURN_VOID(test_call());
     return 0;
 }
--- c-debug.h
+++ c-debug.h
@@ -5,8 +5,9 @@
 extern "C" {
 #endif
 
-#define C_DEBUG_PRINT_X32(x) printf("[%s:%s:%d] @(0x%08x)%s = 0x%x\n", __FILE__, __FUNCTION__, __LINE__, (unsigned int) &x, #x, x)
-#define C_DEBUG_PRINT_CALL(x) printf("[%s:%s:%d] %s\n", __FILE__, __FUNCTION__, __LINE__, #x); x
+#define C_DEBUG_PRINT_X32(x) printf("[%s:%s:%d] @(0x%08x) %s = 0x%x\n", __FILE__, __FUNCTION__, __LINE__, (unsigned int) &x, #x, x)
+#define C_DEBUG_PRINT_CALL_RETURN_VOID(x) printf("[%s:%s:%d] %s\n", __FILE__, __FUNCTION__, __LINE__, #x); x
+#define C_DEBUG_PRINT_CALL_RETURN_X32(x) printf("[%s:%s:%d] %s = 0x%x\n", __FILE__, __FUNCTION__, __LINE__, #x, x)
 #ifdef __cplusplus
 }
 #endif
